1d2828f631f481431c0e51d35d8ab153ce5579f1
mleejee <mikeleja.alt@gmail.com>
2020-10-09 11:03:22 -0400

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
 import React from "react";
 import "./style.css";
 
-import labelText from "./labelText.json";
+import {ColorPicker, NumericTextBox} from '@progress/kendo-react-inputs';
 
 //const stuff = {label:"Super Duper"};
 //const stuff = require("./labelText.json");
 export default function App() {
   return (
     <div>
-        <fieldset style={{width: "50%"}}>
+        <fieldset>
           <legend>Borders</legend>
-          <div class="grid-container">
-            <div class="item1"></div>
-            <div class="item2">
-              <label>{labelText.borders.top}</label>
-              <input type="checkbox" name="top"/>
+          <div className="grid-container">
+            <div className="item1"></div>
+            <div className="item2">
+              <label>Top</label>
+              <input type="checkbox" />
             </div>
-            <div class="item3"></div>
+            <div className="item3"></div>
+            <div className="item4">
+              <label>Color</label>
+              <ColorPicker view="gradient" defaultValue={'white'} />
+            </div>
 
           
-            <div class="item1">
-              <label>{labelText.borders.left}</label>
+            <div className="item1">
+              <label>Left</label>
               <input type="checkbox" name="left"/>
             </div>
-            <div class="item2"></div>
-            <div class="item3">
-              <label>{labelText.borders.right}</label>
+            <div className="item2"></div>
+            <div className="item3">
+              <label>Right</label>
               <input type="checkbox" name="right"/>
             </div>
           
 
-            <div class="item1"></div>
-            <div class="item2">
-            <label>{labelText.borders.bottom}</label>
+            <div className="item1"></div>
+            <div className="item2">
+            <label>Bottom</label>
             <input type="checkbox" name="bottom"/>
             </div>
-            <div class="item3"></div>
+            <div className="item3"></div>
+            <div className="item4">
+              <label>Width</label>
+            </div>
           </div>
          </fieldset>
       
